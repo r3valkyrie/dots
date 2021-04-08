fbbacba97e4eaf2bf28b2d88ddc48109007b5c95
valkyrie <valkyrie@r3valkyrie.com>
2021-04-08 11:21:04 -0400

    split some files

--- zsh/valk.zsh
+++ zsh/valk.zsh
@@ -29,35 +29,9 @@ precmd() { vcs_info }
 
 PROMPT='%~${vcs_info_msg_0_} $ '
 
-# Aliases
-alias vi='vim'
-alias weather='curl wttr.in'
-alias py='python'
-alias cp='cp -i'
+# Run tmux
+if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
+    exec tmux;
+fi
 
-alias t='tmux attach || tmux new-session'
-alias ta='tmux attach -t'
-alias tn='tmux new-session'
-alias tls='tmux list-sessions'
-alias trs='tmux rename-session'
-alias tks='tmux kill-session -t'
-alias tka='rm /tmp/.init && tmux kill-server'
-
-alias ls='ls --color=auto' # Colorize ls output
-alias grep='grep --color' # Same with grep
-alias ll='ls -la' # Use long listing format
-alias l.='ls -d .* --color=auto' # Show only hidden files
-
-alias gg="git grep --color"
-alias gs="git status"
-alias gsi="git status -uno"
-alias ga="git add"
-alias grm="git rm"
-alias gc="git commit -v"
-alias gb="git branch"
-alias gp="git pull"
-alias gr="git reset --hard HEAD"
-alias gco="git checkout"
-alias gpm="git push origin master"
-
-cdir() { mkdir -p "$1"; cd "$1"; }
+command task
